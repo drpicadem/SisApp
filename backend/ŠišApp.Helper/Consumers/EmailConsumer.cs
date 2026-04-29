f5b68a822d35b91976dc86b06134f6ffe3630bd9
Adem <adem.drpic@edu.fit.ba>
2026-04-29 14:47:28 +0200

    Refactor EmailConsumer.cs

--- backend/ŠišApp.Helper/Consumers/EmailConsumer.cs
+++ backend/ŠišApp.Helper/Consumers/EmailConsumer.cs
@@ -6,16 +6,6 @@ using MailKit.Net.Smtp;
 using MimeKit;
 using ŠišApp.Contracts;
 
-namespace ŠišApp.Contracts
-{
-    public class SendEmailEvent
-    {
-        public string ToEmail { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
-    }
-}
-
 namespace ŠišApp.Helper.Consumers
 {
     public class EmailConsumer : IConsumer<SendEmailEvent>
